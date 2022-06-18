2887e40b93633bac40e2fca532ee370d0076a98e
alisalim17 <alisalimli1899@gmail.com>
2022-06-18 12:02:02 +0400

    mutation

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
+from calendar import Calendar
 from ariadne.asgi import GraphQL
 from ariadne import ObjectType, make_executable_schema
+from ariadne import MutationType
 
 type_defs = """
+    type User {
+        username: String!
+    }
+    
     type Query {
+        holidays(year: Int): String!
         hello: String!
         user: User
     }
 
-    type User {
-        username: String!
+    
+    type Mutation {
+        login(username: String!, password: String!): Boolean!
+        logout: Boolean!
     }
-"""
 
+    
+"""
+mutation = MutationType()
 query = ObjectType("Query")
+
+
+@mutation.field('login')
+def resolve_login(_, info, username, password):
+    request = info.context["request"]
+
+    if request["is_authenticated"] and username == "Ali":
+        return True
+    return False
+
+
+@mutation.field('logout')
+def resolve_logout(_, info):
+    request = info.context["request"]
+    if request.is_authenticated:
+        # auth.logout(request)
+        # some logout process
+        return True
+    return False
+
+
+@query.field("holidays")
+def resolve_holidays(*_, year=None):
+    print(year)
+    return year
 
 
 @query.field("user")
@@ -28,5 +64,9 @@ def resolve_username(obj, *_):
     return f'{obj["first_name"]} {obj["last_name"]}'
 
 
-schema = make_executable_schema(type_defs, query, user)
-app = GraphQL(schema, debug=True)
+def get_context_value(request):
+    return {'request': {"is_authenticated": True}, 'test': "TEST"}
+
+
+schema = make_executable_schema(type_defs, query, mutation, user)
+app = GraphQL(schema, context_value=get_context_value, debug=True)
